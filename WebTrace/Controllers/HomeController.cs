84fed9c0de4c902012ae40d20bc2db9cbc5d3bf9
Adam Drewery <adam.drewery@live.co.uk>
2016-05-05 15:22:10 +0100

    Got rid of old stuff

--- WebTrace/Controllers/HomeController.cs
+++ WebTrace/Controllers/HomeController.cs
@@ -3,26 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.SignalR;
 
 namespace WebTrace.Controllers
 {
-    public class LogController : HubController
+    public class LogController : Hub
+    {
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+    }
 
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        [Route("")]
-        [HttpPost]
-        public IActionResult ReceiveMessage()
-        {
-            return Ok();
-        }
-
-        public IActionResult Error()
         {
             return View();
         }
